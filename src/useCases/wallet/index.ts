639e3eff0ceba14c3257f63120b43f752447990d
Isaque Hernandes <isantana@delirec.com>
2023-02-01 23:01:10 -0300

    fix wallet useCases dependency injection

--- src/useCases/wallet/index.ts
+++ src/useCases/wallet/index.ts
@@ -1,26 +1,20 @@
 import { walletDAO } from "@dataAccess/index";
-import validator from "@validations/validator";
 
 import makeWalletList from "./makeWalletList";
 import makeWalletCreate from "./makeWalletCreate";
 import makeWalletUpdate from "./makeWalletUpdate";
 import makeWalletRemove from "./makeWalletRemove";
-import { walletValidations } from "@models/wallet";
 
 const getWalletList = makeWalletList({
   walletDAO
 })
 
 const postWalletCreate = makeWalletCreate({
-  walletDAO,
-  validator: validator,
-  validationRules: walletValidations.changeValidation
+  walletDAO
 })
 
 const putWalletUpdate = makeWalletUpdate({
-  walletDAO,
-  validator: validator,
-  validationRules: walletValidations.changeValidation
+  walletDAO
 })
 
 const deleteWalletRemove = makeWalletRemove({
