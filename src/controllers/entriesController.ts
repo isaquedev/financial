bc649feeee067ce283237faff6e1b58ee7f44cf1
Isaque Hernandes <isantana@delirec.com>
2023-02-02 00:00:57 -0300

    entry routes and controller

--- src/controllers/entriesController.ts
+++ src/controllers/entriesController.ts
@@ -1,12 +1,9 @@
-import { HttpRequest, HttpResponse } from "../adapters/routeAdapter";
+import { deleteEntryRemove, getEntryList, postEntryCreate, putEntryUpdate } from "@useCases/entry";
+import { HttpRequest } from "../adapters/routeAdapter";
 
 export default Object.freeze({
-  getAll: async (request: HttpRequest): Promise<HttpResponse> => {
-    return {
-      statusCode: 200,
-      body: {
-        message: `Hello ${request.query.name}`
-      }
-    }
-  }
+  getAll: (request: HttpRequest) => getEntryList(request),
+  create: (request: HttpRequest) => postEntryCreate(request),
+  update: (request: HttpRequest) => putEntryUpdate(request),
+  remove: (request: HttpRequest) => deleteEntryRemove(request),
 })
--- src/routes/entries.ts
+++ src/routes/entries.ts
@@ -1,9 +1,28 @@
 import express from 'express';
+
 import routeAdapter from '@adapters/routeAdapter';
+import validateRequestAdapter from '@adapters/validateRequestAdapter';
+import middlewareAdapter from '@adapters/middlewareAdapter';
+
 import entriesController from '@controllers/entriesController';
+
+import { isAuthenticated, validateRequest } from '@middlewares/index';
+
+import { entryValidations } from '@models/entry';
 
 const router = express.Router();
 
-router.get("/", routeAdapter(entriesController.getAll))
+router.get("/", middlewareAdapter(isAuthenticated), routeAdapter(entriesController.getAll))
+router.post("/",
+  middlewareAdapter(isAuthenticated),
+  validateRequestAdapter(validateRequest, entryValidations.changeValidation),
+  routeAdapter(entriesController.create)
+)
+router.put("/:id",
+  middlewareAdapter(isAuthenticated),
+  validateRequestAdapter(validateRequest, entryValidations.changeValidation),
+  routeAdapter(entriesController.update)
+)
+router.delete("/:id", middlewareAdapter(isAuthenticated), routeAdapter(entriesController.remove))
 
 export default router
